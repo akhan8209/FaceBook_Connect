cabbf14f63c292ad049946d96e8c46885196a1c7
New <default@gmail.com>
2017-10-06 23:10:28 +0500

    initial

---
+++ FaceBook_Connect/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FaceBook_Connect.Startup))]
+namespace FaceBook_Connect
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
